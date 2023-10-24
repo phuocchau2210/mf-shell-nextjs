6b0eb594f85627e604a809445d13b06d31d0690f
Tran Nhat Duy <vodiepphuocchau@gmail.com>
2023-10-24 17:50:11 +0700

    fix: use script tag for hotjar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,12 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { useEffect } from "react";
-import Hotjar from "@hotjar/browser";
+// import Hotjar from "@hotjar/browser";
 
 export default function App({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    Hotjar.init(3707330, 6);
-  }, []);
+  // useEffect(() => {
+  //   Hotjar.init(3707330, 6);
+  // }, []);
 
   return <Component {...pageProps} />;
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -26,6 +26,18 @@ class MyDocument extends Document {
             name="google-site-verification"
             content="KBgJmLjn6773WnejjhuVndDbj5330wVY-DXTpBtx0CE"
           />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3707330,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+            }}
+          />
         </Head>
         <body>
           <Main />
