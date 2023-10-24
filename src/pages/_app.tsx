ce0b669d2712bda3cf8c7e1e81a683bf0a05441f
Tran Nhat Duy <vodiepphuocchau@gmail.com>
2023-10-24 17:37:11 +0700

    feat: add hotjar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import { hotjar } from "react-hotjar";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    hotjar.initialize(3707330, 6);
+  }, []);
+
   return <Component {...pageProps} />;
 }
